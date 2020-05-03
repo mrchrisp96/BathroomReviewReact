39f98a4b976c3cdad41d89bdd12c54aa52bb8045
Christopher Park <basket123rocks@gmail.com>
2020-05-02 23:45:56 -0400

    new commit

--- src/main/java/servlet/bathroomReviewResults.java
+++ src/main/java/servlet/bathroomReviewResults.java
@@ -95,6 +95,7 @@ private void PrintBody (PrintWriter out)
     out.println("       border: 1px solid black;");
     out.println("       padding: 10px;");
     out.println("       text-align: left;");
+    out.println("       width: 100%;");
     out.println("       table-layout: fixed;");
     out.println("   }");
     out.println("   hr.rounded {");
