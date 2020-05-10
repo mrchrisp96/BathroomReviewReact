d0800564aa8bf4e3263613107f613ddf12cd7386
Christopher Park <basket123rocks@gmail.com>
2020-05-10 19:29:23 -0400

    new commit

--- src/main/java/servlet/App.js
+++ src/main/java/servlet/App.js
@@ -1,15 +1,11 @@
-var PrintTemplate = require('react-print');
 var ReactDOM = require('react-dom');
 var React = require('react');
 
 var MyTemplate = React.createClass({
     render() {
         return (
-            <PrintTemplate>
-                <p>Your custom</p>
-                <span>print stuff goes</span>
-                <h1>Here</h1>
-            </PrintTemplate>
+            <p>Your custom</p>
+            <h1>Here</h1>
         );
     }
 });
