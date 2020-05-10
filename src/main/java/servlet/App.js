40787752b03a7e59d57848df2e07f046c9b66747
Christopher Park <basket123rocks@gmail.com>
2020-05-10 18:59:47 -0400

    new commit

--- src/main/java/servlet/App.js
+++ src/main/java/servlet/App.js
@@ -10,6 +10,6 @@ function App() {
     </div>
   );
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+                
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(element, document.getElementById('root'));
